23466fa76b4fcc5ad32a29ecf706cda1e8292359
David Thrane Christiansen <dtc@galois.com>
2019-01-16 14:29:24 -0800

    Redo main as .hs

---
+++ src/Main.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import qualified Data.Aeson as JSON
+
+import JSONRPC
+
+import Cryptol.REPL.Monad
+
+main :: IO ()
+main =
+  serveStdIONS =<< mkApp (0 :: Integer) [("increment", Command $ \s params -> return (s + 1, JSON.toJSON (s + 1)))]

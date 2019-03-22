b4822413db4cd53da6075f3d8a9e754a3abfd300
David Thrane Christiansen <dtc@galois.com>
2019-03-21 17:37:41 -0700

    All Cryptol names to be imported directly into Python

--- python/cryptol-api_test.py
+++ python/cryptol-api_test.py
@@ -16,3 +16,8 @@ val = c.evaluate_expression("x").result()
 
 assert c.call('add', b'\0', b'\1').result() == b'\x01'
 assert c.call('add', bytes.fromhex('ff'), bytes.fromhex('03')).result() == bytes.fromhex('02')
+
+
+cryptol.add_cryptol_module('Foo', c)
+from Foo import *
+assert add(b'\2', 2) == b'\4'

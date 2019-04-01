6fac425e9de07a6e1ed85db16ef1ccdea39b8e42
David Thrane Christiansen <dtc@galois.com>
2019-04-01 09:48:58 -0700

    Add the trivial main program for SAW API

--- saw-remote-api/saw-remote-api/Main.hs
+++ saw-remote-api/saw-remote-api/Main.hs
@@ -1,4 +1,60 @@
 module Main where
 
+import Control.Applicative
+import Control.Exception
+import Control.Lens hiding ((.=))
+import Control.Monad (ap)
+import Control.Monad.IO.Class (MonadIO(..))
+import qualified Data.Aeson as JSON
+import Data.Aeson ((.:), (.=))
+import qualified Data.Aeson.Types as JSON
+import Data.Text (Text)
+import qualified Options.Applicative as Opt
+import System.Directory (doesDirectoryExist, setCurrentDirectory)
+import System.IO (stdout)
+
+import Argo
+import Argo.Socket
+import Argo.HistoryWrapper
+import Argo.CacheTree
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main =
+  do opts <- Opt.execParser options
+     realMain opts
+
+
+data Options =
+  Options
+    { transportOpt :: TransportOpt
+    }
+
+newtype Port = Port String
+
+data TransportOpt = StdIONetstring | SocketNetstring Port
+
+options :: Opt.ParserInfo Options
+options = Opt.info (Options <$> transport) (Opt.fullDesc)
+
+transport :: Opt.Parser TransportOpt
+transport = socket <|> stdio
+  where
+    socket = SocketNetstring . Port <$> Opt.strOption (Opt.long "socket" <> Opt.metavar "PORT" )
+    stdio = Opt.flag StdIONetstring StdIONetstring (Opt.long "stdio" <> Opt.help "Use netstrings over stdio")
+
+
+validateServerState :: () -> IO Bool
+validateServerState () = pure True
+
+realMain :: Options -> IO ()
+realMain opts =
+  do initSt <- pure () -- initialState
+     cache  <- newCache initSt
+     theApp <- mkApp (HistoryWrapper cache) (historyWrapper validateServerState sawMethods)
+     case transportOpt opts of
+       StdIONetstring -> serveStdIONS theApp
+       SocketNetstring (Port p) -> serveSocket (Just stdout) "127.0.0.1" p theApp
+
+sawMethods :: [(Text, MethodType, JSON.Value -> Method () JSON.Value)]
+sawMethods =
+  []

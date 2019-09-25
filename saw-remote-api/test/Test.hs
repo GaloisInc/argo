c1edbe8e263614e8387acbb3acd524b303671acd
Kenny Foner <kwf@galois.com>
2019-09-25 15:01:26 -0400

    Add integration test code for testing scripting API

--- saw-remote-api/test/Test.hs
+++ saw-remote-api/test/Test.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Main where
 
 import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.HUnit.ScriptExit
+import Paths_saw_remote_api (getDataFileName)
 
 main :: IO ()
-main = defaultMain tests
-
-
-
-tests :: TestTree
-tests = testGroup "Tests for saw-remote-api" []
+main = do
+  testScriptsDir <- getDataFileName "test-scripts/"
+  scriptTests <- makeScriptTests testScriptsDir [python3]
+  defaultMain $
+    testGroup "Tests for saw-remote-api"
+      [testGroup "Scripting API tests" scriptTests]

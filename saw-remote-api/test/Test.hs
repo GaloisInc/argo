7be660c94d94563421de0e69e90d9439a773127b
David Thrane Christiansen <dtc@galois.com>
2019-09-29 18:35:07 -0700

    Create dummy Haskell package that contains the Python API
    This is because cabal sdist doesn't support including files from
    outside a particular package's source tree as data files. This
    provides a small way for other Haskell libraries (or their test
    suites) to gain easy access to the necessary Python files.

--- saw-remote-api/test/Test.hs
+++ saw-remote-api/test/Test.hs
@@ -2,17 +2,19 @@
 
 module Main where
 
+import Argo.PythonBindings
+
 import System.FilePath ((</>))
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.HUnit.ScriptExit
-import Paths_saw_remote_api (getDataFileName)
+import Paths_saw_remote_api
 
 main :: IO ()
 main =
-  do reqs <- getDataFileName "../python/requirements.txt"
+  do reqs <- getArgoPythonFile "requirements.txt"
      withPython3venv (Just reqs) $ \pip python ->
-       do pySrc <- getDataFileName "../python"
+       do pySrc <- getArgoPythonFile "."
           testScriptsDir <- getDataFileName "test-scripts/"
           pip ["install", pySrc]
 

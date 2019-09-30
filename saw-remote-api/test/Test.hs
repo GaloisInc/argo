1a22ad67a06ade68a92564f2f6fc1cf7c10b8131
David Thrane Christiansen <dtc@galois.com>
2019-09-29 18:23:26 -0700

    Abstract virtualenv environment out of saw-remote-api test suite
    This makes it available in the cryptol-remote-api test suite as
    well.

--- saw-remote-api/test/Test.hs
+++ saw-remote-api/test/Test.hs
@@ -3,37 +3,21 @@
 module Main where
 
 import System.FilePath ((</>))
-import System.Process (system)
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.HUnit.ScriptExit
 import Paths_saw_remote_api (getDataFileName)
 
 main :: IO ()
-main = do
-  venvDir <- getDataFileName "virtenv/"
-  system $ "python3 -m venv \"" ++ venvDir ++ "\""
+main =
+  do reqs <- getDataFileName "../python/requirements.txt"
+     withPython3venv (Just reqs) $ \pip python ->
+       do pySrc <- getDataFileName "../python"
+          testScriptsDir <- getDataFileName "test-scripts/"
+          pip ["install", pySrc]
 
-  reqs <- getDataFileName "../python/requirements.txt"
-  pySrc <- getDataFileName "../python"
+          scriptTests <- makeScriptTests testScriptsDir [python]
 
-
-  testScriptsDir <- getDataFileName "test-scripts/"
-
-  system $ (venvDir </> "bin" </> "pip") ++ " install -r \"" ++ reqs ++ "\""
-
-  system $ (venvDir </> "bin" </> "pip") ++ " install  \"" ++ pySrc ++ "\""
-
-  let venvPython =
-        TestLang
-          { testLangName       = "Python in virtualenv"
-          , testLangExtension  = ".py"
-          , testLangExecutable = venvDir </> "bin" </> "python"
-          , testLangArgsFormat = \file -> [file]
-          }
-
-
-  scriptTests <- makeScriptTests testScriptsDir [venvPython]
-  defaultMain $
-    testGroup "Tests for saw-remote-api"
-      [testGroup "Scripting API tests" scriptTests]
+          defaultMain $
+            testGroup "Tests for saw-remote-api"
+              [testGroup "Scripting API tests" scriptTests]

8559a7d0a7d35f26637e5387149d0384de3a5b1a
David Thrane Christiansen <dtc@galois.com>
2019-10-09 17:03:35 -0700

    Add test to verify the constant function that returns 7

---
+++ saw-remote-api/test-scripts/seven.py
@@ -0,0 +1,17 @@
+import os
+import os.path
+import saw
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+c = saw.connect("cabal new-exec --verbose=0 saw-remote-api -- --dynamic4")
+
+seven_bc = os.path.join(dir_path, 'seven.bc')
+
+c.llvm_load_module('m', seven_bc).result()
+
+c.llvm_start_setup('setup').result()
+c.llvm_return("7 : [32]").result()
+c.llvm_finish_setup().result()
+
+print(c.llvm_verify('m', 'seven', [], False, 'setup', 'abc', 'ok').result())

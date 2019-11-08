c3551abb0f1e9c26200e023c32d419a1b6513710
David Thrane Christiansen <dtc@galois.com>
2019-11-08 10:58:45 -0800

    Plausible Contract type in Python

--- python/saw/llvm.py
+++ python/saw/llvm.py
@@ -1,6 +1,10 @@
 from abc import ABCMeta, abstractmethod
+from cryptol import cryptoltypes
+from dataclasses import dataclass, field
+import pprint
 import re
-from typing import Any, List, Optional, Set
+from typing import Any, List, Optional, Set, Union
+from typing_extensions import Literal
 
 class LLVMType(metaclass=ABCMeta):
     @abstractmethod
@@ -27,17 +31,47 @@ class SetupVal(metaclass=ABCMeta):
     @abstractmethod
     def to_json(self) -> Any: pass
 
-class FreshVar(SetupVal):
+class SetupTerm(SetupVal):
+    pass
+
+class CryptolTerm(SetupTerm):
+    expression : cryptoltypes.CryptolJSON
+
+    def __init__(self, code : Union[str, cryptoltypes.CryptolJSON]):
+        if isinstance(code, str):
+            self.expression = cryptoltypes.CryptolLiteral(code)
+        else:
+            self.expression = code
+
+    def __call__(self, *args : cryptoltypes.CryptolJSON) -> 'CryptolTerm':
+        out_term = self.expression
+        for a in args:
+            out_term = cryptoltypes.CryptolApplication(out_term, a)
+
+        return CryptolTerm(out_term)
+
+    def to_json(self) -> Any:
+        return cryptoltypes.to_cryptol(self.expression)
+
+    def __to_cryptol__(self, _ty : Any) -> Any:
+        return self.expression
+
+class FreshVar(SetupTerm):
     name : Optional[str]
 
-    def __init__(self, spec : Spec, type : LLVMType) -> None:
+    def __init__(self, spec : 'Contract', type : LLVMType) -> None:
         self.name = None
         self.spec = spec
         self.type = type
 
+    def __to_cryptol__(self, ty : Any) -> Any:
+        if self.name is None:
+            self.name = self.spec.get_fresh_name()
+        return cryptoltypes.CryptolLiteral(self.name).__to_cryptol__(ty)
+
     def to_json(self) -> Any:
         if self.name is None:
-            raise ValueError('No assigned name for decl')
+            self.name = self.spec.get_fresh_name()
         return {'fresh': self.name,
                 'type': self.type.to_json()}
 
@@ -48,6 +82,7 @@ def next_name(x : str) -> str:
     if match is None:
         return 'x'
     prefix, number = match.group('prefix', 'number')
+
     if prefix is None:
         prefix = 'x'
     if number is None:
@@ -74,26 +109,65 @@ class PointsTo(Prop):
     def to_json(self) -> Any:
         return {'pointer': self.pointer.to_json(), 'target': self.target.to_json()}
 
-class Spec:
+@dataclass
+class Allocated:
+    type : LLVMType
+    name : Optional[str] = None
+
+    def to_json(self) -> Any:
+        return {'name': self.name, 'type': self.type.to_json()}
+
+@dataclass
+class State:
+    contract : 'Contract'
+    fresh : List[FreshVar] = field(default_factory=list)
+    conditions : List[Prop] = field(default_factory=list)
+    allocated : List[Allocated] = field(default_factory=list)
+    points_to : List[PointsTo] = field(default_factory=list)
+
+    def to_json(self) -> Any:
+        return {'variables': [v.to_json() for v in self.fresh],
+                'conditions': [c.to_json() for c in self.conditions],
+                'allocated': [a.to_json() for a in self.allocated],
+                'points to': [p.to_json() for p in self.points_to]
+               }
+
+ContractState = \
+  Union[Literal['pre'],
+        Literal['call'],
+        Literal['post'],
+        Literal['done']]
+
+@dataclass
+class Void:
+    def to_json(self) -> Any:
+        return None
+
+void = Void()
+
+class Contract:
     __used_names : Set[str]
 
-    __pre_props : Set[Prop]
-    __post_props : Set[Prop]
+    __state : ContractState = 'pre'
 
-    __current_props : Optional[Set[Prop]]
+    __pre_state : State
+    __post_state : State
+
+    __returns : Optional[Union[SetupVal, Void]]
 
     __arguments : Optional[List[SetupVal]]
 
+    __in_post : bool
+
     def __init__(self) -> None:
+        self.__pre_state = State(self)
+        self.__post_state = State(self)
         self.__used_names = set()
-        self.__pre_props = set()
-        self.__post_props = set()
-        self.__current_props = None
         self.__arguments = None
+        self.__returns = None
+        self.__in_post = False
 
     # To be overridden by users
-    def setup(self) -> None:
-        pass
     def pre(self) -> None:
         pass
     def call(self) -> None:
@@ -101,19 +175,44 @@ class Spec:
     def post(self) -> None:
         pass
 
+    def get_fresh_name(self, hint : str = 'x') -> str:
+        new_name = uniquify(hint, self.__used_names)
+        self.__used_names.add(new_name)
+        return new_name
+
+    def add_default_var_names(self) -> None:
+        for x in self.__dict__:
+            if isinstance(self.__dict__[x], FreshVar) and self.__dict__[x].name is None:
+                new_name = uniquify(x, self.__used_names)
+                self.__dict__[x].name = new_name
+                self.__used_names.add(new_name)
+
+    def cryptol(self, data : Any) -> CryptolTerm:
+        return CryptolTerm(data)
 
 
-    def declare(self, type : LLVMType) -> SetupVal:
-        return FreshVar(self, type)
+    def declare(self, type : LLVMType) -> FreshVar:
+        v = FreshVar(self, type)
+        if self.__state == 'pre':
+            self.__pre_state.fresh.append(v)
+        elif self.__state == 'post':
+            self.__post_state.fresh.append(v)
+        else:
+            raise Exception("wrong state")
+        return v
+
 
     def declare_pointer(self, type : LLVMType) -> SetupVal:
         return FreshVar(self, LLVMPointerType(type))
 
     def points_to(self, pointer : SetupVal, target : SetupVal) -> None:
-        if self.__current_props is None:
-            raise ValueError("Not in pre- or post-condition setup")
+        pt = PointsTo(pointer, target)
+        if self.__state == 'pre':
+            self.__pre_state.points_to.append(pt)
+        elif self.__state == 'post':
+            self.__post_state.points_to.append(pt)
         else:
-            self.__current_props.add(PointsTo(pointer, target))
+            raise Exception("wrong state")
 
     def arguments(self, *args : SetupVal) -> None:
         if self.__arguments is not None:
@@ -121,26 +220,40 @@ class Spec:
         else:
             self.__arguments = [arg for arg in args]
 
+    def returns(self, val : Union[Void,SetupVal]) -> None:
+        if self.__state == 'post':
+            if self.__returns is None:
+                self.__returns = val
+            else:
+                raise ValueError("Return value already specified")
+        else:
+            raise ValueError("Not in postcondition")
+
+
     def contract(self) -> Any:
-        self.setup()
-        for x in self.__dict__:
-            if isinstance(self.__dict__[x], FreshVar) and self.__dict__[x].name is None:
-                new_name = uniquify(x, self.__used_names)
-                self.__dict__[x].name = new_name
-                self.__used_names.add(new_name)
-
-        self.__current_props = self.__pre_props
+        if self.__state != 'pre':
+            raise Exception("Wrong state")
         self.pre()
+        self.add_default_var_names()
 
-        self.__current_props = None
+        self.__state = 'call'
         self.call()
 
-        self.__current_props = self.__post_props
+        self.__state = 'post'
         self.post()
+        self.add_default_var_names()
 
-        return {'vars' : [d.to_json()
-                          for d in self.__dict__.values()
-                          if isinstance(d, SetupVal)]}
+        self.__state = 'done'
+
+        if self.__returns is None:
+            raise Exception("forgot return")
+
+        return {'pre': self.__pre_state.to_json(),
+                'arguments': [a.to_json() for a in self.__arguments] if self.__arguments is not None else [],
+                'post': self.__post_state.to_json(),
+                'returns': self.__returns.to_json()}
+
+
 
 
 # data Contract cryptolExpr =
@@ -158,21 +271,32 @@ class Spec:
 #     }
 
 
-class Incr(Spec):
-    def setup(self) -> None:
+class Incr(Contract):
+    def pre(self) -> None:
         self.x = self.declare(uint32_t)
 
-print(Incr().contract())
+    def call(self) -> None:
+        self.arguments(self.x)
 
-class Swap(Spec):
-    def setup(self) -> None:
-        t = uint32_t
-        self.x = self.declare(t)
-        self.y = self.declare(t)
-        self.x_pointer = self.declare_pointer(t)
-        self.y_pointer = self.declare_pointer(t)
+    def post(self) -> None:
+        add = self.cryptol("(+)")
+        self.returns(add(self.x, self.cryptol(1)))
+
+pprint.pprint(Incr().contract())
+
+print('--------------------------')
+
+class Swap(Contract):
+    t : LLVMType
+    def __init__(self) -> None:
+        super().__init__()
+        self.t = uint32_t
 
     def pre(self) -> None:
+        self.x = self.declare(self.t)
+        self.y = self.declare(self.t)
+        self.x_pointer = self.declare_pointer(self.t)
+        self.y_pointer = self.declare_pointer(self.t)
         self.points_to(self.x_pointer, self.x)
         self.points_to(self.y_pointer, self.y)
 
@@ -182,6 +306,7 @@ class Swap(Spec):
     def post(self) -> None:
         self.points_to(self.x_pointer, self.y)
         self.points_to(self.y_pointer, self.x)
+        self.returns(void)
 
 
-print(Swap().contract())
+pprint.pprint(Swap().contract())

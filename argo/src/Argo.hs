ceee23df8f9daf8f28e5993e679b73c04f29e4a1
Kenny Foner <kwf@galois.com>
2019-11-14 12:57:39 -0500

    Revert "Define zoomMethod for translating between methods with different state types"
    This reverts commit a3ca27e45269f8b8895aa4a022b65ab54e1118f8.

--- argo/src/Argo.hs
+++ argo/src/Argo.hs
@@ -9,8 +9,6 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE RankNTypes #-}
 {-# OPTIONS_GHC -Wno-redundant-constraints -Wno-name-shadowing #-}
 
 -- | An implementation of the basic primitives of JSON-RPC 2.0.
@@ -23,7 +21,6 @@ module Argo
   , Method(..)
   , runMethod
   , method
-  , zoomMethod
   -- * Manipulating state in methods
   , getState
   , modifyState
@@ -57,7 +54,6 @@ import Control.Monad.Reader
 import Control.Monad.State
 import Control.Exception hiding (TypeError)
 import Control.Lens hiding ((.=))
-import Control.Lens.Zoom (zoom)
 import qualified Data.Aeson as JSON
 import Data.Aeson ((.:), (.:!), (.=))
 import qualified Data.Aeson.Types as JSON (Parser, typeMismatch)
@@ -87,12 +83,7 @@ jsonRPCVersion = "2.0"
 -- to a JSON value representing its response.
 newtype Method state result
   = Method (ReaderT (Text -> IO ()) (StateT state IO) result)
-  deriving (Functor, Applicative, Monad, MonadIO, MonadState state)
-
--- | Given a lens between a smaller and larger state, run a method in the
--- smaller state within the context of the larger state.
-zoomMethod :: Lens' s t -> Method t a -> Method s a
-zoomMethod l (Method m) = Method (zoom l m)
+  deriving (Functor, Applicative, Monad, MonadIO)
 
 runMethod :: Method state result -> (Text -> IO ()) -> state -> IO (state, result)
 runMethod (Method m) log s = swap <$> runStateT (runReaderT m log) s

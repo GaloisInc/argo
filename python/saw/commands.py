c063a0ec0fb52dd0495ddda3560df8856226d5b5
Kenny Foner <kwf@galois.com>
2019-11-25 14:47:09 -0500

    Override process_error and modify process_result for LLVMVerify command
    * process_result and process_erorr now append their verification results to the
    connection's list of verification results
    * process_error handles all verification errors and returns False rather than
    throwing an exception

--- python/saw/commands.py
+++ python/saw/commands.py
@@ -2,7 +2,7 @@ import argo.interaction
 import inspect
 from argo.interaction import HasProtocolState
 
-from typing import Any, List, Optional, Tuple
+from typing import Any, List, Optional, Tuple, Dict
 
 class CryptolLoadFile(argo.interaction.Command):
     def __init__(self, connection : HasProtocolState, filename : str) -> None:
@@ -67,6 +67,7 @@ class LLVMVerify(argo.interaction.Command):
             setup : Any,
             tactic : str,
             lemma_name : str,
+            verifications : Optional[List[Tuple[str, int, Dict[str, str], bool]]] = None,
             location : Optional[Tuple[str, int]] = None) -> None:
         params = {'module': module,
                   'function': function,
@@ -75,14 +76,32 @@ class LLVMVerify(argo.interaction.Command):
                   'contract': setup,
                   'tactic': tactic,
                   'lemma name': lemma_name}
+        self.params = params
+        # Determine the location of this verification call: either where the
+        # command was created, or the passed-in location
+        self.filename : Optional[str] = None
+        self.line     : Optional[int] = None
         if location is None:
-            calling_frame = inspect.currentframe().f_back
-            self.filename = calling_frame.f_code.co_filename
-            self.line     = calling_frame.f_lineno
+            calling_frame = inspect.currentframe()
+            if calling_frame is not None:
+                self.filename = calling_frame.f_back.f_code.co_filename
+                self.line     = calling_frame.f_back.f_lineno
         else:
             self.filename = location[0]
             self.line     = location[1]
+        self.verifications = verifications
         super(LLVMVerify, self).__init__('SAW/LLVM/verify', params, connection)
 
     def process_result(self, _res : Any) -> Any:
-        return None
+        if self.verifications is not None:
+            if self.filename is not None and self.line is not None:
+                self.verifications.append((self.filename, self.line, self.params, True))
+        return True
+
+    def process_error(self, code : int, data : Any, msg : str) -> Any:
+        if (self.verifications is not None) and ((code >= 10300) and (code < 20000)):
+            if self.filename is not None and self.line is not None:
+                self.verifications.append((self.filename, self.line, self.params, False))
+            return False
+        else:
+            return None

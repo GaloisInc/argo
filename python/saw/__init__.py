4326a37ad94a5055d5bcbaab6cd54ec6895e6a9c
Kenny Foner <kwf@galois.com>
2019-11-25 14:49:16 -0500

    Thread verification log through SAWConnection with human-friendly logs
    Passing the optional argument 'delay_verification_exceptions = True' to
    `saw.connect()` will make it so that calls to `llvm_verify()` return boolean
    results rather than throwing exceptions on false results of verification. When
    operating in this mode, the `summary()`, `readable_summary()`, and
    `assert_all_verifications()` methods become useful (otherwise they do nothing
    and return `None`). A script in this mode can be ended with
    `c.assert_all_verifications()` and an assertion error with pretty-printed
    summary will be thrown iff any prior verifications for the session `c` were
    false. This summary can also be non-exceptionally inspected using the
    `readable_summary()` method.

--- python/saw/__init__.py
+++ python/saw/__init__.py
@@ -9,10 +9,11 @@ from saw.commands import *
 from typing import Optional
 
 
-def connect(command : str, cryptol_path : Optional[str] = None) -> SAWConnection:
+def connect(command : str,
+            delay_verification_exceptions : Optional[bool] = False) -> SAWConnection:
     proc = ac.ServerProcess(command)
     conn = ac.ServerConnection(proc)
-    return SAWConnection(conn)
+    return SAWConnection(conn, delay_verification_exceptions)
 
 
 class SAWConnection:
@@ -20,9 +21,14 @@ class SAWConnection:
 
     most_recent_result : Optional[argo.interaction.Interaction]
 
-    def __init__(self, server_connection : ac.ServerConnection) -> None:
+    def __init__(self,
+                 server_connection : ac.ServerConnection,
+                 delay_verification_exceptions : Optional[bool] = False) -> None:
         self.most_recent_result = None
         self.server_connection = server_connection
+        self._verifications : Optional[List[Tuple[str, int, Dict[str, str], bool]]] = None
+        if delay_verification_exceptions:
+            self._verifications = []
 
     def snapshot(self) -> SAWConnection:
         """Return a ``SAWConnection`` that has the same process and state as
@@ -31,7 +37,46 @@ class SAWConnection:
         """
         copy = SAWConnection(self.server_connection)
         copy.most_recent_result = self.most_recent_result
+        if self._verifications is not None:
+            copy._verifications = self._verifications[:] # shallow copy of list spine
         return copy
+
+    def summary(self) ->  Optional[List[Tuple[str, int, Dict[str, str], bool]]]:
+        """Return a summary of the verification results from this session so far, in
+        the form of a list of (optional filename, optional line number, method
+        parameters, verification result) tuples
+        """
+        if self._verifications is not None:
+            return self._verifications[:]
+        else:
+            return None
+
+    def assert_all_verifications(self) -> None:
+        """Assert that all verifications so far have been correct; if not, throw an
+        AssertionError containing the human-readable summary of the verifications"""
+        readable_summary = self.readable_summary()
+        if readable_summary is not None:
+            (ok, summary) = readable_summary
+            if not ok:
+                raise AssertionError(summary)
+
+    def readable_summary(self) -> Optional[Tuple[bool, str]]:
+        """Return a human-readable summary of the verification results from this
+        session so far.
+        """
+        if self._verifications is None:
+            return None
+        ok = True
+        output = ""
+        for (filename, line, params, result) in self._verifications:
+            ok = ok and result
+            if result:
+                mark = '\033[1;32m✔\033[1;0m'
+            else:
+                mark = '\033[1;31m✘\033[1;0m'
+            output_line = mark + " " + filename + ":" + str(line) + ": '" + params['lemma name'] + "'"
+            output += output_line + "\n"
+        return (ok, output)
 
     def protocol_state(self) -> Any:
         if self.most_recent_result is None:
@@ -56,14 +101,19 @@ class SAWConnection:
                     contract : Any,
                     tactic : str,
                     lemma_name : str) -> argo.interaction.Command:
-        # find location of function call
-        calling_frame = inspect.currentframe().f_back
-        filename = calling_frame.f_code.co_filename
-        line     = calling_frame.f_lineno
-        location = (filename, line)
+        # Determine the location of this verification call: either where the
+        # command was created, or the passed-in location
+        filename : Optional[str] = None
+        line     : Optional[int] = None
+        location : Optional[Tuple[str, int]] = None
+        calling_frame = inspect.currentframe()
+        if calling_frame is not None:
+            filename = calling_frame.f_back.f_code.co_filename
+            line     = calling_frame.f_back.f_lineno
+            location = (filename, line)
 
         self.most_recent_result = \
-            LLVMVerify(self, module, function, lemmas, check_sat, contract, tactic, lemma_name, location)
+            LLVMVerify(self, module, function, lemmas, check_sat, contract, tactic, lemma_name, self._verifications, location)
         return self.most_recent_result
 
     def llvm_assume(self,

f8493a64f865a1f0f5c3fe150eb16af2f4e8089f
David Thrane Christiansen <dtc@galois.com>
2020-03-26 15:39:30 -0700

    Add a --public flag to bind a public socket
    While doing this, the substantially similar main actions in both
    servers were consolidated into one, reducing duplication, and the
    usage information blurb was greatly improved.

--- saw-remote-api/saw-remote-api/Main.hs
+++ saw-remote-api/saw-remote-api/Main.hs
@@ -1,17 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import Control.Applicative
-import Control.Concurrent.Async (wait)
 import qualified Data.Aeson as JSON
 import Data.Text (Text)
-import qualified Options.Applicative as Opt
-import System.IO (BufferMode(..), hSetBuffering, stdout)
 
 import Argo
-import Argo.Socket
-import Argo.HistoryWrapper
 import Argo.CacheTree
+import Argo.DefaultMain
+import Argo.HistoryWrapper
 
 import SAWServer
 import SAWServer.CryptolSetup
@@ -23,53 +19,14 @@ import SAWServer.SaveTerm
 
 main :: IO ()
 main =
-  do opts <- Opt.execParser options
-     realMain opts
-
-
-data Options =
-  Options
-    { transportOpt :: TransportOpt
-    }
-
-newtype Port = Port String
-
-data TransportOpt
-  = StdIONetstring              -- ^ NetStrings over standard IO
-  | SocketNetstring Port        -- ^ NetStrings over specific port (all hosts)
-  | SocketNetstringDyn String   -- ^ NetStrings over specific host (dynamic port)
-
-options :: Opt.ParserInfo Options
-options = Opt.info (Options <$> transport) (Opt.fullDesc)
-
-transport :: Opt.Parser TransportOpt
-transport = dyn4 <|> dyn6 <|> socket <|> stdio <|> pure StdIONetstring
-  where
-    socket      = SocketNetstring . Port <$>
-                    Opt.strOption (Opt.long "socket" <> Opt.metavar "PORT" )
-
-    stdio       = Opt.flag' StdIONetstring (Opt.long "stdio" <> Opt.help "Use netstrings over stdio")
-
-    dyn4        = Opt.flag' (SocketNetstringDyn "127.0.0.1") (Opt.long "dynamic4")
-
-    dyn6        = Opt.flag' (SocketNetstringDyn "::1"      ) (Opt.long "dynamic")
-
-
-
-
-realMain :: Options -> IO ()
-realMain opts =
   do initSt <- initialState
      cache  <- newCache initSt
      theApp <- mkApp (HistoryWrapper cache) (historyWrapper validateSAWState sawMethods)
-     case transportOpt opts of
-       StdIONetstring -> serveStdIONS theApp
-       SocketNetstring (Port p) -> serveSocket "127.0.0.1" p theApp
-       SocketNetstringDyn h ->
-         do hSetBuffering stdout NoBuffering
-            (a, p) <- serveSocketDynamic h theApp
-            putStrLn ("PORT " ++ show p)
-            wait a
+     defaultMain description theApp
+
+description :: String
+description =
+  "An RPC server for SAW."
 
 sawMethods :: [(Text, MethodType, JSON.Value -> Method SAWState JSON.Value)]
 sawMethods =

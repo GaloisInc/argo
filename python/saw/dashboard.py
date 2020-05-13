b9b81b68263cab9ec9a1ae526c950ee6a471d145
Kenny Foner <kwf@galois.com>
2020-05-13 12:51:38 -0400

    Fix syntax error in generated dashboard HTML

--- python/saw/dashboard.py
+++ python/saw/dashboard.py
@@ -9,39 +9,38 @@ from . import View, VerificationResult, VerificationFailed
 MYXINE_PORT = 1123
 
 # Loading SVG is licensed for free reuse from https://icons8.com/preloaders/
-LOADING_SVG = \
-    """<svg xmlns:svg="http://www.w3.org/2000/svg"
-    xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
-    version="1.0" width="35pt" height="35pt" viewBox="0 0 128 128"
-    xml:space="preserve">
-    <g><path d="M59.6 0h8v40h-8V0z" fill="#000"/>
-    <path d="M59.6 0h8v40h-8V0z" fill="#ccc" transform="rotate(30 64 64)"/>
-    <path d="M59.6 0h8v40h-8V0z" fill="#ccc" transform="rotate(60 64 64)"/>
-    <path d="M59.6 0h8v40h-8V0z" fill="#ccc" transform="rotate(90 64 64)"/>
-    <path d="M59.6 0h8v40h-8V0z" fill="#ccc" transform="rotate(120 64 64)"/>
-    <path d="M59.6 0h8v40h-8V0z" fill="#b2b2b2" transform="rotate(150 64 64)"/>
-    <path d="M59.6 0h8v40h-8V0z" fill="#999" transform="rotate(180 64 64)"/>
-    <path d="M59.6 0h8v40h-8V0z" fill="#7f7f7f" transform="rotate(210 64 64)"/>
-    <path d="M59.6 0h8v40h-8V0z" fill="#666" transform="rotate(240 64 64)"/>
-    <path d="M59.6 0h8v40h-8V0z" fill="#4c4c4c" transform="rotate(270 64 64)"/>
-    <path d="M59.6 0h8v40h-8V0z" fill="#333" transform="rotate(300 64 64)"/>
-    <path d="M59.6 0h8v40h-8V0z" fill="#191919" transform="rotate(330 64 64)"/>
-    <animateTransform attributeName="transform" type="rotate"
-    values="0 64 64;30 64 64;60 64 64;90 64 64;120 64 64;150 64 64;180 64 64;210 64 64;240 64 64;270 64 64;300 64 64;330 64 64"
-    calcMode="discrete" dur="1320ms" repeatCount="indefinite">
-    </animateTransform></g></svg>"""
+LOADING_SVG = '' # \
+    # """<svg xmlns:svg="http://www.w3.org/2000/svg"
+    # xmlns="http://www.w3.org/2000/svg"
+    # xmlns:xlink="http://www.w3.org/1999/xlink"
+    # version="1.0" width="35pt" height="35pt" viewBox="0 0 128 128"
+    # xml:space="preserve">
+    # <g><path d="M59.6 0h8v40h-8V0z" fill="#000"/>
+    # <path d="M59.6 0h8v40h-8V0z" fill="#ccc" transform="rotate(30 64 64)"/>
+    # <path d="M59.6 0h8v40h-8V0z" fill="#ccc" transform="rotate(60 64 64)"/>
+    # <path d="M59.6 0h8v40h-8V0z" fill="#ccc" transform="rotate(90 64 64)"/>
+    # <path d="M59.6 0h8v40h-8V0z" fill="#ccc" transform="rotate(120 64 64)"/>
+    # <path d="M59.6 0h8v40h-8V0z" fill="#b2b2b2" transform="rotate(150 64 64)"/>
+    # <path d="M59.6 0h8v40h-8V0z" fill="#999" transform="rotate(180 64 64)"/>
+    # <path d="M59.6 0h8v40h-8V0z" fill="#7f7f7f" transform="rotate(210 64 64)"/>
+    # <path d="M59.6 0h8v40h-8V0z" fill="#666" transform="rotate(240 64 64)"/>
+    # <path d="M59.6 0h8v40h-8V0z" fill="#4c4c4c" transform="rotate(270 64 64)"/>
+    # <path d="M59.6 0h8v40h-8V0z" fill="#333" transform="rotate(300 64 64)"/>
+    # <path d="M59.6 0h8v40h-8V0z" fill="#191919" transform="rotate(330 64 64)"/>
+    # <animateTransform attributeName="transform" type="rotate"
+    # values="0 64 64;30 64 64;60 64 64;90 64 64;120 64 64;150 64 64;180 64 64;210 64 64;240 64 64;270 64 64;300 64 64;330 64 64"
+    # calcMode="discrete" dur="1320ms" repeatCount="indefinite">
+    # </animateTransform></g></svg>"""
 
 
 def serve_self_refreshing(path: str, title: str, content: str) -> None:
     url = 'http://localhost:' + str(MYXINE_PORT) + '/' + path.strip('/')
-    wrapped_content = '<div id="content">' + content + '</div>'
     requests.post(url=url,
                   params={
                       'title': title,
-                      'refresh': 'set',
+                      # 'refresh': 'set',
                   },
-                  data=wrapped_content.encode("utf-8"))
+                  data=content.encode("utf-8"))
 
 
 class Dashboard(View):
@@ -132,7 +131,8 @@ class Dashboard(View):
                 cleaned.append(line)
                 break
         for line in lines:
-            cleaned.append(line)
+            if not line.startswith('<!--'):
+                cleaned.append(line)
         return '\n'.join(cleaned)
 
     def errors_html(self) -> str:
@@ -169,7 +169,6 @@ class Dashboard(View):
                 progress += \
                     '🚫 <span style="font-size: 25pt">' \
                     '(incomplete: exception during proving)</span>'
-            progress += '</div>'
         else:
             progress += LOADING_SVG \
                 + '<br/><span style="font-size: 25pt">' \

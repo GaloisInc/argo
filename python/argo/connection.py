cac9287318d02b2e0931aed5a0a814af8acce1b6
Aaron Tomb <atomb@galois.com>
2020-10-08 16:15:04 -0700

    Attempt more robust server loading

--- python/argo/connection.py
+++ python/argo/connection.py
@@ -203,13 +203,11 @@ class DynamicSocketProcess(SocketProcess):
 
             if self.proc.stdout is None:
                 raise ValueError("Server process has no stdout")
-            out_line = self.proc.stdout.readline()
-            match = re.match(r'PORT (\d+)', out_line)
-            if match:
-                self.port = int(match.group(1))
-            else:
-                raise Exception("Failed to load process, output was `" +
-                                out_line + "' but expected PORT then a port.")
+            match = False
+            while !match:
+              out_line = self.proc.stdout.readline()
+              match = re.match(r'PORT (\d+)', out_line)
+            self.port = int(match.group(1))
 
         self.socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
         self.socket.connect(("localhost", self.port))

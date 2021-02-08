453f1a2b53b02991c3d872c645a5a30812cf59ac
Andrew Kent <andrew@galois.com>
2021-02-08 14:31:46 -0800

    chore: more cleaning

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='argo',
       version='0.0.1',
-      packages=['argo', 'cryptol', 'saw'],
+      packages=['argo'],
       package_data={
           '': ['py.typed']
       }
